e46d49c2dfcc3ecbb7241c4760d670bd33d4fc47
shisan-fei <1340673657@qq.com>
2021-09-12 12:12:04 +0800

    updata

--- learn/异常/异常处理.py
+++ learn/异常/异常处理.py
@@ -1,8 +1,8 @@
-with open('test','a') as test:
+with open('test', 'a') as test:
     test.write('aaaaaabbbbb' + '\n')
 
 try:  #尝试打开文件，病读取
-    with open('test','r') as tfile:
+    with open('test', 'r') as tfile:
         tfiles = tfile.read()
 except FileNotFoundError:   #遇到FileNotFoundError异常打印语句。
     print('you find file not exit')
