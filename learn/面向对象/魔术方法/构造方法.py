f87d28172a219ff32a1cea9e352d538058ed7498
shisan-fei <1340673657@qq.com>
2021-09-19 10:20:52 +0800

    构造方法

--- learn/面向对象/魔术方法/构造方法.py
+++ learn/面向对象/魔术方法/构造方法.py
@@ -1,18 +1,17 @@
 '''
-        __new__ 构造方法
-        触发：实例化对象时自动触发，在__init__之前触发
+__new__ 构造方法：
+        触发：当类实例化对象时自动触发，在__init__之前触发
         作用：管理控制对象创建过程
         参数：一个cls接收当前类，其他参数根据初始化方法参数决定
         返回值：必须返回object.__new__(cls)，若没有返回值，实例化对象结果为none
         注意事项：__new__ 方法的参数和 __init__方法参数保持一致，除了第一个参数
-        应用场景：设计模式中单例模式
+        应用场景：设计模式中单例模式（一个类只能被实例化为一个对象）
 '''
 #定义一个人类
 class Person():
     #构造方法
     def __new__(cls, *args, **kwargs):
         '''
-
         :param args: 接受初始化方法的参数，由Person传入
         :param kwargs:
         '''
@@ -23,11 +22,12 @@ class Person():
         return object.__new__(cls)
 
     #初始化方法
-    def __init__(self,name,age,sex):
+    def __init__(self, name, age, sex):
         print('触发初始化方法')
         self.name = name
         self.age = age
         self.sex = sex
+    #__call__方法可以将对象当做函数使用，默认执行当前方法
     def __call__(self, *args, **kwargs):
         print('你把对象当做函数调用了')
     #析构方法
