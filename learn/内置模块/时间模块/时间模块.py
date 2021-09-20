a7ceb4dd71f2ef20600d1fb415a4245b5a1f92c3
shisan-fei <1340673657@qq.com>
2021-09-20 12:06:56 +0800

    时间模块

--- learn/内置模块/时间模块/时间模块.py
+++ learn/内置模块/时间模块/时间模块.py
@@ -5,10 +5,11 @@ ctime()  获取当前时间
 localtime() 获取时间元祖
 strftime()  格式化时间
 sleep(秒数)  在给定时间内暂停线程
+perf_counter() 程序运行的时间
 '''
-print(time.time())
+print(time.time())     #--->1632110476.2288897
 print(time.ctime())    #--->Sat Sep 11 17:31:02 2021
-print(time.localtime())
+print(time.localtime())  #
 
 
 #localtime获取的时间格式化 xx年xx月xx日
@@ -18,3 +19,5 @@ print(f'{res.tm_year}年{res.tm_mon}月{res.tm_mday}日{res.tm_hour}时')
 print(time.strftime('%Y-%m-%d-%H-%M-%S-%w'))     #2021-09-11-17-58-22-6
 time.sleep(5)
 print(time.strftime('%Y-%m-%d-%H-%M-%S-%w'))     #2021-09-11-17-58-27-6
+print(time.perf_counter_ns())
+print(time.perf_counter())         #程序运行的时间
