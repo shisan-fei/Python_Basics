3e40b47f9414c86e6dad24bfa6ec64e31ab0d4e6
shisan-fei <1340673657@qq.com>
2021-09-25 17:14:07 +0800

    web留言板

--- learn/数据库使用/web留言板/留言板显示.py
+++ learn/数据库使用/web留言板/留言板显示.py
@@ -1,8 +1,8 @@
 import pymysql
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
-#留言列表
+#定义视图，留言列表
 @app.route('/')
 def index():
     #获取所有留言板数据
@@ -14,7 +14,21 @@ def index():
 '''调用render_template，会找到index.html文件，将data作为data写入
     index.html文件在当前目录下的template目录下
     '''
+#定义视图，显示留言添加页面
+@app.route('/add')
+def add():
+    #显示添加页面
+    return render_template('add.html')
 
+#定义视图函数，接收表单数据，完成数据写入
+@app.route('/insert',methods=['POST'])
+def insert():
+    #接收表单数据
+    data = request.form.to_dict()
+    print(data)
+    #将数据添加到数据库
+    #页面跳转到留言列表页
+    return '接收表单，写入数据库'
 #封装一个mysql操作
 def model(sql):
     try:
@@ -34,7 +48,6 @@ def model(sql):
             return row
     finally:
         db.close()
-
 
 
 if  __name__ == '__main__':
