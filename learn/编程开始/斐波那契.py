71803dd1de11c06b8aec016a611b82d740228fb0
shisan-fei <1340673657@qq.com>
2021-09-26 13:51:26 +0800

    斐波那契

--- learn/编程开始/斐波那契.py
+++ learn/编程开始/斐波那契.py
@@ -1,8 +1,15 @@
 #斐波那契数列：从第二个数开始，后一个数是前两个数之和
 # a = 0
 # b = 1
-a, b = 0 ,1    #将0赋值给a ，1赋值给b
-while b<100:
-    print(b,end=' ')   #输出b，不换行
-    a,b = b, a+b       #b赋值给a：a就是前一个数，a+b就是下一个数（a a+b b）
+# a, b = 0 ,1    #将0赋值给a ，1赋值给b
+# while b<100:
+#     print(b,end=' ')   #输出b，不换行
+#     a,b = b, a+b       #b赋值给a：a就是前一个数，a+b就是下一个数（a a+b b）
 
+def fb(n):
+    a,b=0,1
+    while b<n:
+        print(b,end=" ")
+        a,b = b,a+b
+
+fb(10)
