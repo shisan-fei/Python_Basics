312c6454d59a328729ff13fc9574802c6e86da42
shisan-fei <1340673657@qq.com>
2021-10-03 11:50:52 +0800

    request的get方法

--- learn/爬虫基础/requests基础/requests_get.py
+++ learn/爬虫基础/requests基础/requests_get.py
@@ -5,6 +5,14 @@ url = 'https://www.baidu.com'
 
 #发起get请求
 res = requests.get(url=url)
+#找github上星级最高python项目
+# url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print('status_code:',r.status_code)
+
+response_dict = r.json()
+print(response_dict.keys())
+print("total repositories:",response_dict['total_count'])
 
 #获取响应结果
 print(res)
